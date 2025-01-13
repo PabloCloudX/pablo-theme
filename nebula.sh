54ca45fc94359122f9698fde9f30166c989f05c7
PabloTzy <135358752+PabloTzy@users.noreply.github.com>
2025-01-13 20:57:57 +0700

    Update nebula.sh

--- nebula.sh
+++ nebula.sh
@@ -11,8 +11,19 @@ echo "Installing Node.js v20..."
 sudo apt-get install -y ca-certificates curl gnupg
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
+# Pastikan konfirmasi otomatis
+if [ $? -eq 0 ]; then
+  echo "GPG key successfully imported!"
+else
+  echo "Error importing GPG key. Retrying with confirmation..."
+  echo "y" | curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+fi
+
+echo "Adding NodeSource repository..."
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-sudo apt-get update
+
+sudo apt-get update -y
 sudo apt-get install -y nodejs
 
 # Install Yarn globally
